cfb49902bf7d0432c27a0cd42801e54b12d22ec3
savlino <savlino@users.noreply.github.com>
2019-04-10 17:39:19 +0300

    Add files via upload

---
+++ 2019.04.10_kashing.js
@@ -0,0 +1,27 @@
+let hash = function(str) {
+	let hash = 0;
+	for (let i = 0; i < str.length; i++) {
+		hash += str.charCodeAt(i) * 2;
+	};
+};
+
+
+let hashFunc = function() {
+	let result = [];
+
+	this.push = function(val) {
+		let curr = hash(val);
+		if (!result[curr]) {
+			result[curr] = val;
+		};
+	};
+
+	this.check = function(val) {
+		let curr = hash(val);
+		if (result[curr]) {
+			return result[curr];
+		} else {
+			return undefined;
+		}
+	};
+}
